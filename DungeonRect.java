447b39371cbc2d4fc2e331c6d2ba5b99ea15c844
Gon <gon27.tk08@gmail.com>
2020-03-09 00:13:55 +0900

    RectAngleクラスを作成

--- DungeonRect.java
+++ DungeonRect.java
@@ -1,12 +1,5 @@
 import java.util.*;
 
-class Rect{
-    public int left;
-    public int top;
-    public int right;
-    public int bottom;
-}
-
 class DungeonRect {
     public int left;
     public int top;
---
+++ RectAngle.java
@@ -0,0 +1,35 @@
+class RectAngle{
+    public int left;
+    public int top;
+    public int right;
+    public int bottom;
+
+    RectAngle(int _left, int _top, int _right, int _bottom){
+        setRectAngle(_left, _top, _right, _bottom);
+    }
+
+    void setRectAngle(int _left, int _top, int _right, int _bottom){
+        left = _left;
+        top = _top;
+        right = _right;
+        bottom = _bottom;
+    }
+
+    public int getWidth() {
+        return (right - left);
+    }
+
+    public int getHeight() {
+        return (bottom - top);
+    }
+
+    @Override
+    public String toString() {
+        return  "left = " + left + 
+                ", top = " + top +
+                ", right = " + right + 
+                ", bottom = " + bottom + 
+                ", width = " + getWidth() + 
+                ", height = " + getHeight();
+    }
+}
